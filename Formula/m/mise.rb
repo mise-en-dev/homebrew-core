53d912e4bb725d48b6f2bf2087df8ad711eb8eec
Anka <runner@Mac-1745684705508.local>
2025-04-26 16:46:53 +0000

    mise 2025.4.10

--- Formula/m/mise.rb
+++ Formula/m/mise.rb
@@ -1,8 +1,8 @@
 class Mise < Formula
   desc "Polyglot runtime manager (asdf rust clone)"
   homepage "https://mise.jdx.dev/"
-  url "https://github.com/jdx/mise/archive/refs/tags/v2025.4.9.tar.gz"
-  sha256 "8ccc2ab67280826529ed1aaad563c1846ddbdde787f0c88ccf19b1fc19ffcdaf"
+  url "https://github.com/jdx/mise/archive/refs/tags/v2025.4.10.tar.gz"
+  sha256 "c02b295883dee034547248d92247f0d23ab40cf454d4b41bea33b721550b18d5"
   license "MIT"
   head "https://github.com/jdx/mise.git", branch: "main"
 
